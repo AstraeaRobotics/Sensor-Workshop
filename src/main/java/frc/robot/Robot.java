c9ad82a4a85c901d77e676dc2604a2433a427024
Siddharth Lohani <me@siddharthlohani.dev>
2022-07-06 17:12:17 -0400

    added stubs for color sensor and limelight

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,94 +5,83 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+  public void robotInit() {}
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
-
-  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+  /* COLOR SENSOR STATION */
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+  public enum ColorChoices {
+    RED, BLUE, OTHER, NONE
+  }
+
+  /**
+   * Proximity to the closest item in front of sensor
+   * @return proximity to the closest ball object in front of the sensor
+   */
+  public int getProximity() { return -1; } 
+
+  /**
+   * Determines which color is closest to the color detected by the sensor
+   * @return The color that is closest to what the color sensor detects (RED, BLUE, OTHER or NONE)
+   */
+  public ColorChoices getDetectedColor() { return null; }
+
+  /**
+   * Logs values to SmartDashboard
+   * @param proximity How close the nearest object is to the sensor
+   * @param color What color is detected by the sensor
+   */
+  public void logToDashboard(int proximity, ColorChoices color) {}
+
+
+
+  /* LIMELIGHT STATION */
+
+  /**
+   * Determines whether or not there is a target (as determined by limelight)
+   * @return whether or not a target is visible in the frame
+   */
+  public boolean existsTarget() { return false; }
+
+  /**
+   * Determines horizontal offset from center of the target (as calculated by limelight)
+   * @return the horizontal offset to the center of the target
+   */
+  public double getHorizontalOffset() { return -1.0; }
+
+  /**
+   * Determines whether or not the robot is aligned with the goal
+   * @return if the robot is aligned with the goal
+   */
+  public boolean isAligned() { return false; }
+
+  /**
+   * Aligns the robot with a target
+   * @param horizontalError the horizontal offset (as reported by limelight)
+   */
+  public void alignRobot(double horizontalError) {}
+
+  /**
+   * Logs important Limelight values to SmartDashboard
+   * @param isAligned whether or not the robot is aligned with the goal
+   * @param existsTarget whether or not a target is visible in the frame
+   * @param horizontalError the horizontal offset to the center of the goal
+   */
+  public void logToDashboard(boolean isAligned, boolean existsTarget, double horizontalError) {} // TODO: log stuff
+
+
 }
