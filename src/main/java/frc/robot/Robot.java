e650ee7d563f8ac031200c6a810d416ee9bf4b27
Photon-Aiden <87028152+Photon-Aiden@users.noreply.github.com>
2022-07-07 11:01:32 -0400

    again...

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -137,10 +137,10 @@ public class Robot extends TimedRobot {
    * @param horizontalError the horizontal offset (as reported by limelight)
    */
   public void alignRobot(double horizontalError) {
-    if (horizontalError < 0 && Math.abs(horizontalError) > 2) {
+    if (horizontalError < 0) {
       rightMotors.set(-.1);
       leftMotors.set(-.1);
-    } else if (horizontalError > 0 && Math.abs(horizontalError) > 2) {
+    } else if (horizontalError > 0) {
       rightMotors.set(0.1);
       leftMotors.set(0.1);
     }
