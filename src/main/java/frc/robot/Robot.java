40fa0cc644cde78911cf22b1b4452637175b9f27
Photon-Aiden <87028152+Photon-Aiden@users.noreply.github.com>
2022-07-12 11:10:36 -0400

    gyro and stuffs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,19 +6,24 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.KebabCaseStrategy;
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ColorSensorV3.RawColor;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
+import edu.wpi.first.wpilibj.SPI;
 
 public class Robot extends TimedRobot {
   //NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -40,13 +45,21 @@ public class Robot extends TimedRobot {
   ColorSensorV3 colorSense = new ColorSensorV3(i2cPort);
 
   RelativeEncoder rightMEnc = rightMaster.getEncoder();
+  RelativeEncoder leftMEnc = leftMaster.getEncoder();
+  
+  AHRS gyro = new AHRS(SPI.Port.kMXP);
+  DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(gyro.getRotation2d());
 
   @Override
   public void robotInit() {
+    gyro.reset();
+    rightMEnc.setPosition(0.0);
+    leftMEnc.setPosition(0.0);
   }
 
   @Override
   public void robotPeriodic() {
+    updateOdometry();
   }
 
   @Override
@@ -61,9 +74,14 @@ public class Robot extends TimedRobot {
     //   rightMotors.set(0);
     //   leftMotors.set(0);
     // }
-    var color = getDetectedColor();
-    var proximity = getProximity();
-    logToDashboard(proximity, color);
+
+    // var color = getDetectedColor();
+    // var proximity = getProximity();
+    // logToDashboard(proximity, color);
+
+    //turnToAngle(90);
+    moveDistance(3);
+    logToDashboard(odometry.getPoseMeters().getX(), odometry.getPoseMeters().getY(), getHeading());
   }
 
   /* COLOR SENSOR STATION */
@@ -98,9 +116,9 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("GREEEEEEEEN", green);
     SmartDashboard.putNumber("BLUEeee", blue);
 
-    if (Math.max(red, blue) == red) {
+    if (red > blue) {
       return ColorChoices.RED;
-    } else if (Math.max(blue, red) == blue) {
+    } else if (blue > red) {
       return ColorChoices.BLUE;
     } else {
       return ColorChoices.NONE;
@@ -110,6 +128,7 @@ public class Robot extends TimedRobot {
   /*
   Emojis go here
   >:)
+  \(^V^)/
   */
 
   
@@ -123,15 +142,18 @@ public class Robot extends TimedRobot {
     switch (color) {
       case BLUE:
         SmartDashboard.putString("Color", "Blue");
+        break;
       case RED:
         SmartDashboard.putString("Color", "Red");
-      case OTHER:
-        SmartDashboard.putString("Color", "Other");
+        break;
       case NONE:
         SmartDashboard.putString("Color", "None");
+        break;
       default:
-        SmartDashboard.putString("Color", "None")
+        SmartDashboard.putString("Color", "None");
+        break;
     }
+    SmartDashboard.putNumber("Proximity", proximity);
   }
 
   /* LIMELIGHT STATION */
@@ -205,9 +227,10 @@ public class Robot extends TimedRobot {
    * 
    * @return the yaw angle in degrees
    */
-  public double getHeading() {
+  public double getHeading() { // Heidi
     // TODO write method
-    return 0.0;
+    double heading = gyro.getAngle();
+    return heading;
   }
 
   /**
@@ -217,6 +240,20 @@ public class Robot extends TimedRobot {
    */
   public void turnToAngle(double degrees) {
     // TODO write method
+    //use getHeading() here
+    double heading = getHeading();
+    double degDiff = heading - degrees;
+    if (degDiff < 0) {
+      // Reminder: Motors are positioned in opposite directions, so yah
+      leftMotors.set(-0.1);
+      rightMotors.set(-0.1);
+    } else if (degDiff > 0) {
+      leftMotors.set(0.1);
+      rightMotors.set(0.1);
+    } else {
+     leftMotors.set(0);
+     rightMotors.set(0);
+    }
   }
 
   /**
@@ -225,16 +262,27 @@ public class Robot extends TimedRobot {
    * @param distance distance in meters (can be negative to move the robot
    *                 backwards)
    */
-  public void moveDistance(double distance) {
-    // TODO write method
+  public void moveDistance(double distance) { //i think we're good
+    // TODO write method <3 <3 <3 <3 <3
+    if (getDistanceTraveled() < Math.abs(distance) && distance > 0) {
+      leftMotors.set(-0.1);
+      rightMotors.set(0.1);
+    } else if (getDistanceTraveled() < Math.abs(distance) && distance < 0) {
+      leftMotors.set(0.1);
+      rightMotors.set(-0.1);
+    } else {
+      leftMotors.set(0);
+      rightMotors.set(0);
+    }
   }
 
   /**
    * Updates the odometry with distance measurements from encoders
    * 
    */
-  public void updateOdometry() {
-    // TODO write method
+  public void updateOdometry() { // Sophia
+    double gearRatio = Math.PI * 0.1524 / 10.81;
+    odometry.update(gyro.getRotation2d(), leftMEnc.getPosition() * gearRatio, rightMEnc.getPosition() * gearRatio);
   }
 
   /**
@@ -242,9 +290,14 @@ public class Robot extends TimedRobot {
    * 
    * @return distance in meters
    */
-  public double getDistanceTraveled() {
+  public double getDistanceTraveled() { // Miriam
     // TODO write method
-    return 0.0;
+    //Use the odometry to get the distance x and y from our starting point 
+    //Then, compute the hypotenuse to get the actual distance
+    double x = odometry.getPoseMeters().getX();
+    double y = odometry.getPoseMeters().getY();
+    double hypotenuse = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    return hypotenuse;
   }
 
   /**
@@ -252,5 +305,8 @@ public class Robot extends TimedRobot {
    */
   public void logToDashboard(double xPos, double yPos, double rotation) {
     // TODO write method
+    SmartDashboard.putNumber("XPOS", xPos);
+    SmartDashboard.putNumber("YPOS", yPos);
+    SmartDashboard.putNumber("Rotation", rotation);
   }
 }
